6c21a0b31d721b8bce700df09bf0753d7b405b3f
Dron <epttit@gmail.com>
2022-07-17 20:04:32 +0300

    add task47

---
+++ task47/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+
+
+double [,] GetSumArray(int m, int n)
+{
+    double [,] variable = new double [m,n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            variable[i,j] = new Random().NextDouble();
+        }
+    }
+    return variable;
+}   
+
+void PrintArray( double [,] array1)
+{
+    for (int i = 0; i < array1.GetLength(0); i++)
+    {
+        for (int j = 0; j < array1.GetLength(1); j++)
+        {
+            Console.Write($"{array1[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int m = 4;
+int n = 4;
+double[,] array = GetSumArray(m, n);
+PrintArray(array);
